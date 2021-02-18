7b5a76c5412a6d370e88b3fd07eeb43c3465d1e3
x-hgg-x <39058530+x-hgg-x@users.noreply.github.com>
2021-02-18 22:10:53 +0100

    Add 2015 day 8

---
+++ src/bin/2015-day8.rs
@@ -0,0 +1,29 @@
+use regex::Regex;
+
+use std::fs;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let input = fs::read_to_string("inputs/2015-day8.txt")?;
+
+    let re = Regex::new(r#"(?m)(?:\\\\|\\"|\\x[0-9A-Fa-f]{2}|^"|"$)"#).unwrap();
+
+    let result1: i32 = re
+        .find_iter(&input)
+        .map(|x| match x.as_str().as_bytes() {
+            br#"""# => 1,
+            br#"\\"# => 1,
+            br#"\""# => 1,
+            &[b'\\', b'x', _, _] => 3,
+            _ => 0,
+        })
+        .sum();
+
+    let result2: usize = input
+        .lines()
+        .map(|line| 2 + line.chars().filter(|&c| c == '"' || c == '\\').count())
+        .sum();
+
+    println!("{}", result1);
+    println!("{}", result2);
+    Ok(())
+}
