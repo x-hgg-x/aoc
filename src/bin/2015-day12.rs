55d40cbd3651886a150bf8c96315f1d617be834b
x-hgg-x <39058530+x-hgg-x@users.noreply.github.com>
2021-02-20 01:33:02 +0100

    Add 2015 day 12

---
+++ src/bin/2015-day12.rs
@@ -0,0 +1,60 @@
+#![allow(clippy::trivial_regex)]
+
+use regex::bytes::Regex;
+
+use std::fs;
+
+fn count(re: &Regex, input: &[u8]) -> i32 {
+    re.find_iter(&input)
+        .map(|x| -> i32 { String::from_utf8_lossy(x.as_bytes()).parse().unwrap() })
+        .sum()
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut input = fs::read("inputs/2015-day12.txt")?;
+
+    let regex_num = Regex::new(r#"-?\d+"#).unwrap();
+    let regex_red = Regex::new(r#":"red""#).unwrap();
+
+    let result1 = count(&regex_num, &input);
+
+    while let Some(x) = regex_red.find_iter(&input).next() {
+        let before = input[..x.start()]
+            .iter()
+            .rev()
+            .enumerate()
+            .scan(-1, |braces, (pos, c)| {
+                match c {
+                    b'{' => *braces += 1,
+                    b'}' => *braces -= 1,
+                    _ => {}
+                };
+                Some((pos, *braces))
+            })
+            .find(|&(_, braces)| braces == 0)
+            .unwrap();
+
+        let after = input[x.end()..]
+            .iter()
+            .enumerate()
+            .scan(1, |braces, (pos, c)| {
+                match c {
+                    b'{' => *braces += 1,
+                    b'}' => *braces -= 1,
+                    _ => {}
+                };
+                Some((pos, *braces))
+            })
+            .find(|&(_, braces)| braces == 0)
+            .unwrap();
+
+        let range = x.start() - before.0..x.end() + after.0;
+        input[range].fill(b' ');
+    }
+
+    let result2 = count(&regex_num, &input);
+
+    println!("{}", result1);
+    println!("{}", result2);
+    Ok(())
+}
