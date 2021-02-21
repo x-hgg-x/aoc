36bcdc85c50e7127d9e47bd0bc1f9779132125cb
x-hgg-x <39058530+x-hgg-x@users.noreply.github.com>
2021-02-21 16:12:53 +0100

    Update 2015 day 11

--- src/bin/2015-day11.rs
+++ src/bin/2015-day11.rs
@@ -1,41 +1,51 @@
 use std::fs;
 
-fn increment(input: &mut [u8]) {
-    let mut carry = 1_u8;
-    for x in input.iter_mut().rev() {
-        if *x + carry <= b'z' {
-            *x += carry;
-            carry = 0;
-        } else {
-            *x = b'a';
-            carry = 1;
+struct Password<'a> {
+    data: &'a mut [u8],
+}
+
+impl<'a> Password<'a> {
+    fn next(&mut self) -> &mut [u8] {
+        let mut carry = 1;
+        for x in self.data.iter_mut().rev() {
+            if *x + carry <= b'z' {
+                *x += carry;
+                carry = 0;
+            } else {
+                *x = b'a';
+                carry = 1;
+            }
         }
+
+        self.data
     }
-}
 
-fn check_password(input: &[u8]) -> bool {
-    let mut iter = input.windows(2).enumerate().filter(|(_, x)| x[0] == x[1]);
+    fn next_valid(&mut self) -> String {
+        loop {
+            let password = self.next();
 
-    let check1 = iter
-        .next()
-        .and_then(|(pos1, _)| iter.last().map(|(pos2, _)| pos2 - pos1 > 1))
-        .filter(|&x| x)
-        .is_some();
+            let mut iter = password
+                .windows(2)
+                .enumerate()
+                .filter(|(_, x)| x[0] == x[1]);
 
-    let check2 = input
-        .windows(3)
-        .any(|x| x[0] + 1 == x[1] && x[1] + 1 == x[2]);
+            let check1 = iter
+                .next()
+                .and_then(|(pos1, _)| iter.last().map(|(pos2, _)| pos2 - pos1 > 1))
+                .filter(|&x| x)
+                .is_some();
 
-    let check3 = !input.iter().any(|&x| x == b'i' || x == b'o' || x == b'l');
+            let check2 = password
+                .windows(3)
+                .any(|x| x[0] + 1 == x[1] && x[1] + 1 == x[2]);
 
-    check1 && check2 && check3
-}
+            let check3 = !password
+                .iter()
+                .any(|&x| x == b'i' || x == b'o' || x == b'l');
 
-fn next_password(input: &mut [u8]) -> String {
-    loop {
-        increment(input);
-        if check_password(input) {
-            break String::from_utf8_lossy(input).into_owned();
+            if check1 && check2 && check3 {
+                return String::from_utf8_lossy(&password).into_owned();
+            }
         }
     }
 }
@@ -46,8 +56,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .as_bytes()
         .to_vec();
 
-    let result1 = next_password(&mut input);
-    let result2 = next_password(&mut input);
+    let mut password_generator = Password { data: &mut input };
+
+    let result1 = password_generator.next_valid();
+    let result2 = password_generator.next_valid();
 
     println!("{}", result1);
     println!("{}", result2);
