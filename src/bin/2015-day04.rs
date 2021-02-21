a824c5bb821db0959bb30c20c38281b156219051
x-hgg-x <39058530+x-hgg-x@users.noreply.github.com>
2021-02-21 15:02:04 +0100

    Update 2015 day 4

--- src/bin/2015-day04.rs
+++ src/bin/2015-day04.rs
@@ -1,26 +1,21 @@
+use md5::Digest;
+
 use std::fs;
+
+fn find_digest<F: Fn(&Digest) -> bool>(input: &str, f: F) -> i32 {
+    (1..)
+        .map(|n| (n, md5::compute(format!("{}{}", input, n))))
+        .find(|(_, digest)| f(digest))
+        .map(|(n, _)| n)
+        .unwrap()
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let input = fs::read_to_string("inputs/2015-day04.txt")?;
     let input = input.trim();
 
-    let mut result1: u64 = 1;
-    loop {
-        let digest = md5::compute(format!("{}{}", input, result1));
-        if digest[..2] == [0, 0] && digest[2] <= 0x0F {
-            break;
-        }
-        result1 += 1;
-    }
-
-    let mut result2: u64 = 1;
-    loop {
-        let digest = md5::compute(format!("{}{}", input, result2));
-        if digest[..3] == [0, 0, 0] {
-            break;
-        }
-        result2 += 1;
-    }
+    let result1 = find_digest(&input, |digest| digest[..2] == [0, 0] && digest[2] <= 0x0F);
+    let result2 = find_digest(&input, |digest| digest[..3] == [0, 0, 0]);
 
     println!("{}", result1);
     println!("{}", result2);
