6e4608ad00c4ded4a8a23ddf7813a706765581ee
x-hgg-x <39058530+x-hgg-x@users.noreply.github.com>
2021-02-21 23:59:43 +0100

    Add 2015 day 19

---
+++ src/bin/2015-day19.rs
@@ -0,0 +1,47 @@
+use itertools::Itertools;
+use regex::bytes::Regex;
+
+use std::fs;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let input = fs::read_to_string("inputs/2015-day19.txt")?;
+    let input = input.trim().as_bytes().to_vec();
+
+    let regex_replacements = Regex::new(r#"(?m)^(\w+) => (\w+)$"#).unwrap();
+    let regex_molecule = Regex::new(r#"(\w+)$"#).unwrap();
+
+    let replacements = regex_replacements
+        .captures_iter(&input)
+        .map(|cap| (cap[1].to_owned(), cap[2].to_owned()))
+        .collect_vec();
+
+    let molecule = regex_molecule.find(&input).map(|x| x.as_bytes()).unwrap();
+
+    let result1 = replacements
+        .iter()
+        .flat_map(|(old, new)| {
+            Regex::new(&String::from_utf8_lossy(old))
+                .unwrap()
+                .find_iter(&molecule)
+                .map(|x| {
+                    let mut molecule = molecule.to_vec();
+                    molecule.splice(x.range(), new.clone()).last();
+                    molecule
+                })
+                .collect_vec()
+        })
+        .sorted_unstable()
+        .dedup()
+        .count();
+
+    let molecule = String::from_utf8_lossy(molecule);
+    let length = molecule.matches(|c: char| c.is_ascii_uppercase()).count();
+    let num_y = molecule.matches('Y').count();
+    let num_rn_ar = regex::Regex::new("Rn|Ar")?.find_iter(&molecule).count();
+
+    let result2 = length - num_rn_ar - 2 * num_y - 1;
+
+    println!("{}", result1);
+    println!("{}", result2);
+    Ok(())
+}
