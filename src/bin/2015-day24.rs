6a7945c1ae6a3bc56dea737917b90b384cb83f94
x-hgg-x <39058530+x-hgg-x@users.noreply.github.com>
2021-02-26 18:08:08 +0100

    Add 2015 day 24

---
+++ src/bin/2015-day24.rs
@@ -0,0 +1,93 @@
+use itertools::{Either, Itertools};
+
+use std::fs;
+use std::iter::Map;
+
+fn get_subset_sum_iter(set: &[u64], goal_sum: u64) -> impl Iterator<Item = u64> + '_ {
+    (1..(1u64 << set.len()))
+        .scan((0, 0), move |state, index| {
+            let new_gray = index ^ (index >> 1);
+            let bit_changed = state.1 ^ new_gray;
+            let diff = set[bit_changed.trailing_zeros() as usize];
+            if new_gray & bit_changed == 0 {
+                state.0 -= diff;
+            } else {
+                state.0 += diff;
+            }
+            state.1 = new_gray;
+
+            Some((state.0, new_gray))
+        })
+        .filter(move |&(sum, _)| sum == goal_sum)
+        .map(|(_, bitset)| bitset)
+}
+
+fn get_partition<I: Iterator<Item = u64>>(
+    iter: I,
+    set: &[u64],
+) -> Map<I, impl FnMut(u64) -> (Vec<u64>, Vec<u64>) + '_> {
+    iter.map(move |bitset| -> (Vec<_>, Vec<_>) {
+        (0..set.len()).partition_map(|n| {
+            if bitset & (1 << n) != 0 {
+                Either::Left(set[n as usize])
+            } else {
+                Either::Right(set[n as usize])
+            }
+        })
+    })
+}
+
+fn get_optimal_qe<F: Fn(&[u64]) -> bool>(weights: &[u64], goal_weight: u64, func: F) -> u64 {
+    let valid_subsets = get_subset_sum_iter(&weights, goal_weight).collect_vec();
+
+    let min_length = valid_subsets
+        .iter()
+        .map(|bitset| bitset.count_ones())
+        .min()
+        .unwrap();
+
+    let filtered_set_iter = valid_subsets
+        .into_iter()
+        .filter(move |bitset| bitset.count_ones() == min_length);
+
+    get_partition(filtered_set_iter, weights)
+        .filter(|(_, remaining)| func(remaining))
+        .map(|(first_group, _)| first_group.iter().product())
+        .min()
+        .unwrap()
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let input = fs::read_to_string("inputs/2015-day24.txt")?;
+
+    let weights = input
+        .split_ascii_whitespace()
+        .map(|x| x.parse::<u64>().unwrap())
+        .collect_vec();
+
+    let total_weight: u64 = weights.iter().sum();
+
+    let goal_weight1 = total_weight / 3;
+    let result1 = get_optimal_qe(&weights, goal_weight1, |remaining| {
+        get_subset_sum_iter(remaining, goal_weight1)
+            .next()
+            .is_some()
+    });
+
+    let goal_weight2 = total_weight / 4;
+    let result2 = get_optimal_qe(&weights, goal_weight2, |second_group| {
+        get_partition(
+            get_subset_sum_iter(second_group, goal_weight2),
+            second_group,
+        )
+        .any(|(_, third_group)| {
+            get_subset_sum_iter(&third_group, goal_weight2)
+                .next()
+                .is_some()
+        })
+    });
+
+    println!("{}", result1);
+    println!("{}", result2);
+    Ok(())
+}
