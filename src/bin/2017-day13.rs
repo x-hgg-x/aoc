32cd5fe10bd35a676c94f754891f69808df108e7
x-hgg-x <39058530+x-hgg-x@users.noreply.github.com>
2021-11-10 19:40:10 +0100

    Add 2017 day 13

---
+++ src/bin/2017-day13.rs
@@ -0,0 +1,38 @@
+use eyre::Result;
+use itertools::Itertools;
+
+use std::fs;
+
+struct Layer {
+    depth: usize,
+    range: usize,
+    period: usize,
+}
+
+fn main() -> Result<()> {
+    let input = fs::read_to_string("inputs/2017-day13.txt")?;
+
+    let layers = input
+        .lines()
+        .map(|line| {
+            let mut iter = line.split(": ");
+            let depth = iter.next().unwrap().parse().unwrap();
+            let range = iter.next().unwrap().parse().unwrap();
+            let period = if range == 0 { 0 } else { (range - 1) * 2 };
+            Layer { depth, range, period }
+        })
+        .collect_vec();
+
+    let result1 = layers.iter().filter(|x| x.depth % x.period == 0).map(|x| x.depth * x.range).sum::<usize>();
+
+    let mut delay = 0;
+    while layers.iter().any(|x| (delay + x.depth) % x.period == 0) {
+        delay += 1;
+    }
+
+    let result2 = delay;
+
+    println!("{}", result1);
+    println!("{}", result2);
+    Ok(())
+}
