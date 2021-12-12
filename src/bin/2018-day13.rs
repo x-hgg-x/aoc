986f64f5fc4ad592d0564cdb6ac6725973bafc3a
x-hgg-x <39058530+x-hgg-x@users.noreply.github.com>
2021-12-12 16:12:04 +0100

    Improve performance

--- src/bin/2018-day13.rs
+++ src/bin/2018-day13.rs
@@ -1,8 +1,6 @@
 use eyre::{bail, ensure, Result};
 use num_complex::Complex;
 
-use std::cmp::Ordering;
-use std::collections::btree_map::{BTreeMap, Entry};
 use std::fs;
 use std::iter::repeat;
 
@@ -11,21 +9,6 @@ const NO_TURN: Complex<i64> = Complex::new(1, 0);
 const TURN_RIGHT: Complex<i64> = Complex::new(0, -1);
 const TURNS: [Complex<i64>; 3] = [TURN_LEFT, NO_TURN, TURN_RIGHT];
 
-#[derive(Clone, Copy, PartialEq, Eq)]
-struct CartPosition(Complex<i64>);
-
-impl Ord for CartPosition {
-    fn cmp(&self, other: &Self) -> Ordering {
-        (-self.0.im, self.0.re).cmp(&(-other.0.im, other.0.re))
-    }
-}
-
-impl PartialOrd for CartPosition {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
 struct Cart {
     direction: Complex<i64>,
     turn_index: usize,
@@ -33,11 +16,11 @@ struct Cart {
 
 enum Tile {
     Empty,
-    HorizontalLine,
-    VerticalLine,
-    LeftCurve,
-    RightCurve,
-    Intersection,
+    HorizontalLine(Option<Cart>),
+    VerticalLine(Option<Cart>),
+    LeftCurve(Option<Cart>),
+    RightCurve(Option<Cart>),
+    Intersection(Option<Cart>),
 }
 
 struct Grid {
@@ -54,6 +37,12 @@ impl Grid {
     fn get_index(&self, row: usize, column: usize) -> usize {
         row * self.width + column
     }
+
+    fn get_position(&self, index: usize) -> (usize, usize) {
+        let row = index / self.width;
+        let column = index % self.width;
+        (row, column)
+    }
 }
 
 fn main() -> Result<()> {
@@ -62,91 +51,120 @@ fn main() -> Result<()> {
     let width = input.lines().map(|line| line.len()).max().unwrap();
     let height = input.lines().count();
 
-    let mut tiles = Vec::new();
-    let mut carts = BTreeMap::new();
+    let mut tiles = Vec::with_capacity(width * height);
+    let mut cart_indices = Vec::new();
 
-    input
-        .lines()
-        .map(|line| line.bytes().chain(repeat(b' ')).take(width))
-        .enumerate()
-        .flat_map(|(row_index, row)| {
-            row.enumerate().map(move |(column_index, x)| match x {
-                b' ' => (Tile::Empty, None),
-                b'-' => (Tile::HorizontalLine, None),
-                b'|' => (Tile::VerticalLine, None),
-                b'\\' => (Tile::LeftCurve, None),
-                b'/' => (Tile::RightCurve, None),
-                b'+' => (Tile::Intersection, None),
-                _ => {
-                    let position = Complex::new(column_index as i64, -(row_index as i64));
-                    let turn_index = 0;
-                    match x {
-                        b'<' => (Tile::HorizontalLine, Some((CartPosition(position), Cart { direction: Complex::new(-1, 0), turn_index }))),
-                        b'>' => (Tile::HorizontalLine, Some((CartPosition(position), Cart { direction: Complex::new(1, 0), turn_index }))),
-                        b'^' => (Tile::VerticalLine, Some((CartPosition(position), Cart { direction: Complex::new(0, 1), turn_index }))),
-                        b'v' => (Tile::VerticalLine, Some((CartPosition(position), Cart { direction: Complex::new(0, -1), turn_index }))),
-                        _ => panic!("unknown tile"),
-                    }
+    input.lines().map(|line| line.bytes().chain(repeat(b' ')).take(width)).enumerate().try_for_each(|(row_index, row)| {
+        for (column_index, x) in row.enumerate() {
+            match x {
+                b' ' => tiles.push(Tile::Empty),
+                b'-' => tiles.push(Tile::HorizontalLine(None)),
+                b'|' => tiles.push(Tile::VerticalLine(None)),
+                b'\\' => tiles.push(Tile::LeftCurve(None)),
+                b'/' => tiles.push(Tile::RightCurve(None)),
+                b'+' => tiles.push(Tile::Intersection(None)),
+                b'<' => {
+                    cart_indices.push(row_index * width + column_index);
+                    tiles.push(Tile::HorizontalLine(Some(Cart { direction: Complex::new(-1, 0), turn_index: 0 })));
                 }
-            })
-        })
-        .for_each(|(tile, cart_data): (Tile, Option<(CartPosition, Cart)>)| {
-            tiles.push(tile);
-
-            if let Some((cart_position, cart)) = cart_data {
-                carts.insert(cart_position, cart);
-            }
-        });
-
-    let grid = Grid::new(width, height, tiles)?;
-
-    let mut first_crash_position = None;
-    let mut buf = Vec::with_capacity(carts.len());
-
-    let last_position = loop {
-        buf.clear();
-        buf.extend(carts.keys().copied());
-
-        for cart_position in &buf {
-            if let Some((mut cart_position, mut cart)) = carts.remove_entry(cart_position) {
-                cart_position.0 += cart.direction;
-
-                let index = grid.get_index((-cart_position.0.im) as usize, cart_position.0.re as usize);
-                match grid.tiles[index] {
-                    Tile::HorizontalLine | Tile::VerticalLine => (),
-                    Tile::LeftCurve if cart.direction.im != 0 => cart.direction *= TURN_LEFT,
-                    Tile::LeftCurve if cart.direction.re != 0 => cart.direction *= TURN_RIGHT,
-                    Tile::RightCurve if cart.direction.im != 0 => cart.direction *= TURN_RIGHT,
-                    Tile::RightCurve if cart.direction.re != 0 => cart.direction *= TURN_LEFT,
-                    Tile::Intersection => {
-                        cart.direction *= TURNS[cart.turn_index];
-                        cart.turn_index = (cart.turn_index + 1) % TURNS.len();
-                    }
-                    _ => bail!("unable to follow path at {:?}", cart_position.0),
+                b'>' => {
+                    cart_indices.push(row_index * width + column_index);
+                    tiles.push(Tile::HorizontalLine(Some(Cart { direction: Complex::new(1, 0), turn_index: 0 })));
                 }
-
-                match carts.entry(cart_position) {
-                    Entry::Vacant(entry) => {
-                        entry.insert(cart);
-                    }
-                    Entry::Occupied(entry) => {
-                        entry.remove_entry();
-
-                        if first_crash_position.is_none() {
-                            first_crash_position = Some(cart_position.0);
-                        }
-                    }
-                };
-            }
+                b'^' => {
+                    cart_indices.push(row_index * width + column_index);
+                    tiles.push(Tile::VerticalLine(Some(Cart { direction: Complex::new(0, 1), turn_index: 0 })));
+                }
+                b'v' => {
+                    cart_indices.push(row_index * width + column_index);
+                    tiles.push(Tile::VerticalLine(Some(Cart { direction: Complex::new(0, -1), turn_index: 0 })));
+                }
+                _ => bail!("unknown tile"),
+            };
         }
 
-        if carts.len() == 1 {
-            break carts.keys().last().map(|x| x.0);
+        Ok(())
+    })?;
+
+    let mut grid = Grid::new(width, height, tiles)?;
+
+    let mut first_crash_position = None;
+
+    let last_position = loop {
+        for cart_index in &mut cart_indices {
+            let (mut cart_row, mut cart_column) = grid.get_position(*cart_index);
+
+            let mut cart = match &mut grid.tiles[*cart_index] {
+                Tile::Empty => bail!("empty tile at {:?}", (cart_row, cart_column)),
+                Tile::HorizontalLine(x) | Tile::VerticalLine(x) | Tile::LeftCurve(x) | Tile::RightCurve(x) | Tile::Intersection(x) => match x.take() {
+                    Some(cart) => cart,
+                    None => continue,
+                },
+            };
+
+            cart_row = (cart_row as i64 - cart.direction.im) as usize;
+            cart_column = (cart_column as i64 + cart.direction.re) as usize;
+            *cart_index = grid.get_index(cart_row, cart_column);
+
+            let new_tile_cart = match &mut grid.tiles[*cart_index] {
+                Tile::Empty => bail!("empty tile at {:?}", (cart_row, cart_column)),
+                Tile::HorizontalLine(x) | Tile::VerticalLine(x) => x,
+                Tile::LeftCurve(x) => {
+                    if cart.direction.im != 0 {
+                        cart.direction *= TURN_LEFT;
+                    } else if cart.direction.re != 0 {
+                        cart.direction *= TURN_RIGHT
+                    } else {
+                        bail!("unable to follow path at {:?}", (cart_row, cart_column));
+                    }
+                    x
+                }
+                Tile::RightCurve(x) => {
+                    if cart.direction.im != 0 {
+                        cart.direction *= TURN_RIGHT;
+                    } else if cart.direction.re != 0 {
+                        cart.direction *= TURN_LEFT
+                    } else {
+                        bail!("unable to follow path at {:?}", (cart_row, cart_column));
+                    }
+                    x
+                }
+                Tile::Intersection(x) => {
+                    cart.direction *= TURNS[cart.turn_index];
+                    cart.turn_index = (cart.turn_index + 1) % TURNS.len();
+                    x
+                }
+            };
+
+            match new_tile_cart {
+                None => *new_tile_cart = Some(cart),
+                _ => {
+                    *new_tile_cart = None;
+                    if first_crash_position.is_none() {
+                        first_crash_position = Some((cart_row, cart_column));
+                    }
+                }
+            };
+        }
+
+        cart_indices.sort_unstable();
+
+        cart_indices.retain(|&cart_index| match &grid.tiles[cart_index] {
+            Tile::HorizontalLine(x) => x.is_some(),
+            Tile::VerticalLine(x) => x.is_some(),
+            Tile::LeftCurve(x) => x.is_some(),
+            Tile::RightCurve(x) => x.is_some(),
+            Tile::Intersection(x) => x.is_some(),
+            Tile::Empty => panic!("empty tile at {:?}", grid.get_position(cart_index)),
+        });
+
+        if cart_indices.len() == 1 {
+            break grid.get_position(cart_indices[0]);
         }
     };
 
-    let (x1, y1) = first_crash_position.map(|x| (x.re, -x.im)).unwrap();
-    let (x2, y2) = last_position.map(|x| (x.re, -x.im)).unwrap();
+    let (x1, y1) = first_crash_position.map(|(row, column)| (column, row)).unwrap();
+    let (x2, y2) = (last_position.1, last_position.0);
 
     let result1 = format!("{},{}", x1, y1);
     let result2 = format!("{},{}", x2, y2);
