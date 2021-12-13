f5030fb07fdaf155142ff0e9e5f62cae26e00a8c
x-hgg-x <39058530+x-hgg-x@users.noreply.github.com>
2021-12-13 19:24:34 +0100

    Small fixes

--- src/bin/2015-day06.rs
+++ src/bin/2015-day06.rs
@@ -1,62 +1,80 @@
-use eyre::Result;
+use eyre::{bail, Result};
 use itertools::Itertools;
 use regex::Regex;
 
 use std::fs;
+use std::ops::RangeInclusive;
 
-fn parse_regex<'a>(re: &Regex, text: &'a str) -> (&'a str, usize, usize, usize, usize) {
-    re.captures_iter(text)
-        .map(|x| (x.get(1).unwrap().as_str(), x[2].parse().unwrap(), x[3].parse().unwrap(), x[4].parse().unwrap(), x[5].parse().unwrap()))
-        .next()
-        .unwrap()
+#[derive(Copy, Clone)]
+enum Command {
+    TurnOn,
+    TurnOff,
+    Toogle,
 }
 
-fn set_lights<'a, F, Func>(input: &'a str, re: &Regex, grid: &mut [[i8; 1000]; 1000], f: F)
+struct Instruction {
+    command: Command,
+    line_range: RangeInclusive<usize>,
+    column_range: RangeInclusive<usize>,
+}
+
+fn compute_brightness<F, Func>(grid: &mut [[u8; 1000]], instructions: &[Instruction], f: F) -> u64
 where
-    F: Fn(&'a str) -> Func,
-    Func: Fn(i8) -> i8,
+    F: Fn(Command) -> Func,
+    Func: Fn(u8) -> u8,
 {
-    for line in input.lines() {
-        let (instruction, start_line, start_col, end_line, end_col) = parse_regex(re, line);
+    for instruction in instructions {
+        let func = f(instruction.command);
 
-        let func = f(instruction);
-
-        for grid_line in &mut grid[start_line..=end_line] {
-            for elem in &mut grid_line[start_col..=end_col] {
+        for grid_line in &mut grid[instruction.line_range.clone()] {
+            for elem in &mut grid_line[instruction.column_range.clone()] {
                 *elem = func(*elem);
             }
         }
     }
+
+    grid.iter().flatten().copied().map_into::<u64>().sum()
 }
 
 fn main() -> Result<()> {
     let input = fs::read_to_string("inputs/2015-day06.txt")?;
 
-    let re = Regex::new(r#"(.*?) (\d+),(\d+) through (\d+),(\d+)"#)?;
+    let re = Regex::new(r#"(?m)^(.*?) (\d+),(\d+) through (\d+),(\d+)$"#)?;
+
+    let instructions: Vec<_> = re
+        .captures_iter(&input)
+        .map(|x| -> Result<_> {
+            let command = match &x[1] {
+                "turn on" => Command::TurnOn,
+                "turn off" => Command::TurnOff,
+                "toggle" => Command::Toogle,
+                other => bail!("unknown instruction: {}", other),
+            };
+
+            let line_range = x[2].parse()?..=x[4].parse()?;
+            let column_range = x[3].parse()?..=x[5].parse()?;
+
+            Ok(Instruction { command, line_range, column_range })
+        })
+        .try_collect()?;
 
     let f1 = |instruction| match instruction {
-        "turn on" => |x: i8| x * x,
-        "turn off" => |x: i8| -x * x,
-        "toggle" => |x: i8| -x,
-        other => panic!("unknown instruction: {}", other),
+        Command::TurnOn => |_| 1,
+        Command::TurnOff => |_| 0,
+        Command::Toogle => |x| x ^ 1,
     };
 
     let f2 = |instruction| match instruction {
-        "turn on" => |x: i8| x + 1,
-        "turn off" => |x: i8| (x - 1).max(0),
-        "toggle" => |x: i8| x + 2,
-        other => panic!("unknown instruction: {}", other),
+        Command::TurnOn => |x| x + 1,
+        Command::TurnOff => |x: u8| x.saturating_sub(1),
+        Command::Toogle => |x| x + 2,
     };
 
-    let mut grid = [[-1_i8; 1000]; 1000];
-    set_lights(&input, &re, &mut grid, f1);
+    let mut grid = vec![[0; 1000]; 1000];
+    let result1 = compute_brightness(&mut grid, &instructions, f1);
 
-    let result1 = grid.iter().map(|x| x.iter().filter(|&&x| x == 1).count()).sum::<usize>();
-
-    grid = [[0_i8; 1000]; 1000];
-    set_lights(&input, &re, &mut grid, f2);
-
-    let result2 = grid.iter().map(|x| x.iter().copied().map_into::<i64>().sum::<i64>()).sum::<i64>();
+    grid.fill([0; 1000]);
+    let result2 = compute_brightness(&mut grid, &instructions, f2);
 
     println!("{}", result1);
     println!("{}", result2);
