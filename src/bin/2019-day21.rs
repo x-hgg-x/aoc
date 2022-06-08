fa0597facbdb6bb26da8083c202b45282919671d
x-hgg-x <39058530+x-hgg-x@users.noreply.github.com>
2022-06-08 15:39:13 +0200

    Add 2019 day 21

---
+++ src/bin/2019-day21.rs
@@ -0,0 +1,134 @@
+use aoc::*;
+
+use eyre::bail;
+use itertools::Itertools;
+
+use std::collections::{HashMap, VecDeque};
+
+struct Intcode {
+    program: HashMap<usize, i64>,
+    ip: usize,
+    relative_base: i64,
+    inputs: VecDeque<i64>,
+}
+
+impl Intcode {
+    fn new(program: HashMap<usize, i64>, inputs: VecDeque<i64>) -> Self {
+        Self { program, ip: 0, relative_base: 0, inputs }
+    }
+
+    fn get_input(&mut self, arg_position: usize, instruction: i64) -> Result<i64> {
+        let arg = *self.program.entry(self.ip + arg_position).or_default();
+
+        match instruction / 10i64.pow(1 + arg_position as u32) % 10 {
+            0 => Ok(*self.program.entry(usize::try_from(arg)?).or_default()),
+            1 => Ok(arg),
+            2 => Ok(*self.program.entry(usize::try_from(self.relative_base + arg)?).or_default()),
+            other => bail!("unknown parameter mode: {other}"),
+        }
+    }
+
+    fn get_register(&mut self, arg_position: usize, instruction: i64) -> Result<&mut i64> {
+        let arg = *self.program.entry(self.ip + arg_position).or_default();
+
+        match instruction / 10i64.pow(1 + arg_position as u32) % 10 {
+            0 => Ok(self.program.entry(usize::try_from(arg)?).or_default()),
+            2 => Ok(self.program.entry(usize::try_from(self.relative_base + arg)?).or_default()),
+            other => bail!("invalid parameter mode: {other}"),
+        }
+    }
+
+    fn run(&mut self) -> Result<Vec<i64>> {
+        let mut outputs = Vec::new();
+
+        loop {
+            let instruction = *self.program.entry(self.ip).or_default();
+            match instruction % 100 {
+                1 => {
+                    let arg1 = self.get_input(1, instruction)?;
+                    let arg2 = self.get_input(2, instruction)?;
+                    let arg3 = self.get_register(3, instruction)?;
+                    *arg3 = arg1 + arg2;
+                    self.ip += 4;
+                }
+                2 => {
+                    let arg1 = self.get_input(1, instruction)?;
+                    let arg2 = self.get_input(2, instruction)?;
+                    let arg3 = self.get_register(3, instruction)?;
+                    *arg3 = arg1 * arg2;
+                    self.ip += 4;
+                }
+                3 => {
+                    let input = self.inputs.pop_front().value()?;
+                    let arg1 = self.get_register(1, instruction)?;
+                    *arg1 = input;
+                    self.ip += 2;
+                }
+                4 => {
+                    let arg1 = self.get_input(1, instruction)?;
+                    outputs.push(arg1);
+                    self.ip += 2;
+                }
+                5 => {
+                    let arg1 = self.get_input(1, instruction)?;
+                    let arg2 = self.get_input(2, instruction)?;
+                    if arg1 != 0 {
+                        self.ip = arg2.try_into()?;
+                    } else {
+                        self.ip += 3;
+                    }
+                }
+                6 => {
+                    let arg1 = self.get_input(1, instruction)?;
+                    let arg2 = self.get_input(2, instruction)?;
+                    if arg1 == 0 {
+                        self.ip = arg2.try_into()?;
+                    } else {
+                        self.ip += 3;
+                    }
+                }
+                7 => {
+                    let arg1 = self.get_input(1, instruction)?;
+                    let arg2 = self.get_input(2, instruction)?;
+                    let arg3 = self.get_register(3, instruction)?;
+                    *arg3 = (arg1 < arg2).into();
+                    self.ip += 4;
+                }
+                8 => {
+                    let arg1 = self.get_input(1, instruction)?;
+                    let arg2 = self.get_input(2, instruction)?;
+                    let arg3 = self.get_register(3, instruction)?;
+                    *arg3 = (arg1 == arg2).into();
+                    self.ip += 4;
+                }
+                9 => {
+                    let arg1 = self.get_input(1, instruction)?;
+                    self.relative_base += arg1;
+                    self.ip += 2;
+                }
+                99 => break,
+                other => bail!("unknown opcode: {other}"),
+            }
+        }
+
+        Ok(outputs)
+    }
+}
+
+fn main() -> Result<()> {
+    let input = setup(file!())?;
+    let input = String::from_utf8_lossy(&input);
+    let input = input.trim();
+
+    let program: HashMap<usize, i64> = input.split(',').enumerate().map(|(pos, val)| Result::Ok((pos, val.parse()?))).try_collect()?;
+
+    let inputs1 = (*b"NOT A J\nNOT B T\nOR T J\nNOT C T\nOR T J\nAND D J\nWALK\n").into_iter().map_into().collect();
+    let result1 = *Intcode::new(program.clone(), inputs1).run()?.last().value()?;
+
+    let inputs2 = (*b"NOT A J\nNOT B T\nOR T J\nNOT C T\nOR T J\nAND D J\nNOT E T\nNOT T T\nOR H T\nAND T J\nRUN\n").into_iter().map_into().collect();
+    let result2 = *Intcode::new(program, inputs2).run()?.last().value()?;
+
+    println!("{result1}");
+    println!("{result2}");
+    Ok(())
+}
