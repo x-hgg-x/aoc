86be3a074a79d281354cc79f14d370e4bb7f0ec8
x-hgg-x <39058530+x-hgg-x@users.noreply.github.com>
2023-08-06 22:16:00 +0200

    Add 2022 day 1

---
+++ src/bin/2022-day01.rs
@@ -0,0 +1,22 @@
+use aoc::*;
+
+use itertools::Itertools;
+
+fn main() -> Result<()> {
+    let input = setup(file!())?;
+    let input = String::from_utf8_lossy(&input);
+
+    let calories = input
+        .split("\n\n")
+        .flat_map(|group| group.lines().map(|x| Ok(x.parse::<u64>()?)).try_process(|iter| iter.sum::<u64>()))
+        .sorted_unstable()
+        .collect_vec();
+
+    let (c1, c2, c3) = calories.iter().rev().next_tuple().value()?;
+    let result1 = c1;
+    let result2 = c1 + c2 + c3;
+
+    println!("{result1}");
+    println!("{result2}");
+    Ok(())
+}
